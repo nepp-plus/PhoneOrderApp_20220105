05d02642c052d617b320943eee3e2bc8276806fe
nepp-plus <nepp.plus@gmail.com>
2022-01-05 17:01:28 +0900

    실제 데이터 표시 완료

--- app/src/main/java/com/neppplus/phoneorderapp_20220105/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/neppplus/phoneorderapp_20220105/ViewStoreDetailActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.phoneorderapp_20220105
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.neppplus.phoneorderapp_20220105.models.StoreData
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
@@ -13,6 +14,8 @@ class ViewStoreDetailActivity : AppCompatActivity() {
         val storeData = intent.getSerializableExtra("store") as StoreData
 
         txtStoreName.text = storeData.name
+        txtPhoneNum.text = storeData.phoneNum
+        Glide.with(this).load(storeData.logoURL).into(imgLogo)
 
     }
 }
